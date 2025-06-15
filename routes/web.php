c59076de271ae92917633eb25348c871bf324429
MAIDUGU-jean1 <maidugujeandedieu600@gmail.com>
2025-06-15 16:07:21 +0100

    changes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/index', function () {
